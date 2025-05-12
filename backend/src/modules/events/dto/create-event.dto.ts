42ec86dfe6bcb150ce60bec253999d9093523d49
Beliul Tilahun <tilahunbeliul@gmail.com>
2025-05-12 17:24:36 +0300

    update and create dto created

---
+++ backend/src/modules/events/dto/create-event.dto.ts
@@ -0,0 +1,23 @@
+import { IsString, IsNotEmpty, IsOptional } from 'class-validator';
+
+export class CreateEventDto {
+  @IsString()
+  @IsNotEmpty()
+  title: string;
+
+  @IsString()
+  @IsNotEmpty()
+  date: string;
+
+  @IsString()
+  @IsNotEmpty()
+  time: string;
+
+  @IsString()
+  @IsNotEmpty()
+  location: string;
+
+  @IsString()
+  @IsOptional()
+  imageUri?: string;
+} 
